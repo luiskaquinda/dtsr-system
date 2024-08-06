b53a0c14a920065520a3fb44c0679ac9ee7104e1
Luís Caquinda <luiskaquinda20@gmail.com>
2024-08-06 18:24:34 +0100

    feat: Adicionado o layout ao projecto

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/owner', function () {
+    return view('vehicleowner.home');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
